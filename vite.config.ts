75d9d8389d2c78bacbaf1064206cbeed5e8d536e
Sooyoung Lee <dannylisa67@gmail.com>
2025-05-14 16:54:17 +0900

    [Build] prebuild use esnext

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,17 @@ import wasm from 'vite-plugin-wasm'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths(), wasm()],
+  // pre-bundling 단계(OptimizeDeps)에도 esnext 허용
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext',
+    },
+  },
+  // 프로덕션 번들링 시에도 esnext 문법 그대로 남김
+  esbuild: {
+    target: 'esnext',
+  },
+  build: {
+    target: 'esnext',
+  },
 });
