6eccb16da4b698420713544edd5622560df05a17
Sooyoung Lee <dannylisa67@gmail.com>
2025-05-15 13:18:44 +0900

    explorer website

--- src/components/features/home/input/constants.ts
+++ src/components/features/home/input/constants.ts
@@ -1,11 +1,11 @@
 const PREDICTION_CONSTANTS = {
     dateBase: new Date("2025-05-09"),
-    priceBase: 80000,
+    priceBase: 90000,
     priceStep: 500,
     binBase: -240,
     binCount: 61,
     tickSpacing: 20,
-    explorer: "https://solscan.io/tx/",
+    explorer: "https://solscan.io/",
     network: "devnet",
 }
 
